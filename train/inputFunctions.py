501b1c80c6a20ba31cbb12d2eda16fb40c6bdd7a
Florian Schwab <florian.c.schwab@googlemail.com>
2018-05-10 18:23:35 +0200

    Training can run be run now, but the loss does not go to zero yet

--- train/inputFunctions.py
+++ train/inputFunctions.py
@@ -3,9 +3,16 @@
 #  Number of epochs is defined, number of iterations depends
 #  on the batch size and number of samples in the training set.
 
+from __future__ import print_function
 import os
 import collections
 import csv
+import tensorflow as tf
+import numpy as np
+from tensorflow.contrib.data import batch_and_drop_remainder
+
+FLAGS = tf.app.flags.FLAGS
+
 
 def getImgAndCommandList(recordingsFolder, printInfo = False):
     cmdVelFiles = []
@@ -23,7 +30,7 @@ def getImgAndCommandList(recordingsFolder, printInfo = False):
 
     imgsFolders = collections.OrderedDict(sorted(imgsFolders.items()))
 
-    for imgFolder, imgFiles in imgsFolders.iteritems():
+    for imgFolder, imgFiles in imgsFolders.items():
         imgFiles = sorted(imgFiles)
 
         ibf = os.path.basename(os.path.normpath(imgFolder))  # ibf=ImgBaseFolde
@@ -55,15 +62,15 @@ def getImgAndCommandList(recordingsFolder, printInfo = False):
                         inputList.append(inputDir)
                         
                         if printInfo:
-                            print inputDir
-                            print
+                            print(inputDir)
         else:
-            print "!!! NOT FOUND: ", str(csvFilePath)
-            
-    if not inputList:
+            print("!!! NOT FOUND: ", str(csvFilePath))
+
+    if len(inputList) > 0:
         return inputList
     else:
-        print "!!! NO INPUT"
+        print("!!! NO INPUT")
+
 
 def getCmdDir(path):
     reader = csv.reader(open(path, 'r'))
@@ -83,7 +90,7 @@ def meanCmd(cmdDir, thisImgName, nextImgName, printInfo = False):
     sumValX = 0
     sumValYaw = 0
     countCmds = 0
-    for timestamp, cmd in cmdDir.iteritems():
+    for timestamp, cmd in cmdDir.items():
         if timestamp >= startTimestamp and timestamp < endTimestamp:
             countCmds += 1
             sumValX += float(cmd["valX"])
@@ -93,13 +100,47 @@ def meanCmd(cmdDir, thisImgName, nextImgName, printInfo = False):
     avVelYaw = sumValYaw / countCmds if countCmds > 0 else 0
     
     if printInfo:
-        print "Between ", str(startTimestamp), " and ", str(endTimestamp), "is 1 command:" if countCmds == 1 else " are " + str(countCmds) + " commands:"
-        print "av. velX:    ", str(avVelX)
-        print "av. velYaw:  ", str(avVelYaw)
+        print("Between ", str(startTimestamp), " and ", str(endTimestamp), "is 1 command:" if countCmds == 1 else " are " + str(countCmds) + " commands:")
+        print("av. velX:    ", str(avVelX))
+        print("av. velYaw:  ", str(avVelYaw))
 
     return avVelX, avVelYaw
 
 
+def create_dataset(directory, epochs, batch_size):
+    dataList = getImgAndCommandList(directory)
+
+    # TODO Make this selection configurable by FLAGS
+    filenames = [x["imgPath"] for x in dataList]
+    controls = [x["velYaw"] for x in dataList]
+
+    dataset = tf.data.Dataset.from_tensor_slices((filenames, controls))
+    dataset = dataset.map(load_img)
+    dataset = dataset.shuffle(buffer_size=128)
+    dataset = dataset.apply(batch_and_drop_remainder(batch_size))
+    dataset = dataset.repeat(epochs)
+
+    return dataset, int(np.ceil(len(filenames) / batch_size))
+
+
+def load_img(filename, label):
+    # Load the image with TensorFlow methods
+    image_string = tf.read_file(filename)
+    image_decoded = tf.image.decode_image(image_string, channels=3)
+    image_decoded = tf.expand_dims(image_decoded, axis=0)
+    image_resized = tf.image.resize_bilinear(image_decoded, [FLAGS.height, FLAGS.width])
+    image_resized = tf.squeeze(image_resized, axis=0)
+    # Bring the image to floating point values in range [0.0, 1.0]
+    image_rescaled = tf.image.convert_image_dtype(image_resized, tf.float32)
+
+    return image_rescaled, label
+
+
+def augment_image(filename, label):
+    # TODO Implement later if needed (e.g. random gaussian noise, random left-right flip
+    return filename, label
+
+
 if __name__ == "__main__":
     recordingsFolder = os.path.join(os.path.expanduser("~"), "recordings")
-    getImgAndCommandList(recordingsFolder, printInfo = True)
+    res = getImgAndCommandList(recordingsFolder, printInfo=True)
--- train/models/mobilenet.py
+++ train/models/mobilenet.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers import Reshape, GlobalAveragePooling2D, Dropout, Conv2D, Activation
 
 
-def build_model(input_tensor=None, flags=None):
+def build_model(input_tensor, flags):
     if input_tensor is None:
         input_shape = (flags.height, flags.width, 3)
     else:
@@ -11,16 +12,17 @@ def build_model(input_tensor=None, flags=None):
                                                    input_shape=input_shape,
                                                    input_tensor=input_tensor)
 
+    # Disable training for the convolutional layers
+    for layer in mobnet_basic.layers:
+        layer.trainable = False
+
     # Extend mobile net by own fully connected layer
-    # TODO Evaluate which activation function works best
     x = mobnet_basic.layers[-1].output
-    predictions = tf.keras.layers.Dense(1, activation='linear', name='predictions')(x)
+
+    x = Reshape((1, 1, 7 * 7 * 1024))(x)
+    x = tf.keras.layers.Dense(1, activation='linear', name='predictions')(x)
+    predictions = tf.keras.backend.squeeze(Reshape((1, ))(x), 1)
     mobnet_extended = tf.keras.Model(inputs=input_tensor, outputs=predictions,
                                      name='mobnet_extended')
 
     return mobnet_extended
-
-
-if __name__ == "__main__":
-
-    build_model(input_pl)

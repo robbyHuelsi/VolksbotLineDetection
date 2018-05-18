9d2814026c0de1781d0f483425f3f98c6ae2a8c3
robbyHuelsi <robert@huelsi.de>
2018-05-18 16:25:30 +0200

    gui: thread as class - doesnt work

--- train/gui.py
+++ train/gui.py
@@ -2,61 +2,118 @@ import os
 import json
 from tkinter import *
 from PIL import ImageTk, Image
+import time
+from threading import Thread
 
 import inputFunctions
 
 # View and Controll
 class imgAndCmdWindow():
     def __init__(self, imgAndCommandList):
-        this.imgAndCommandList = imgAndCommandList
-        this.i = 0
-        window = Tk()
-        window.title(str(this.imgAndCommandList[this.i]["imgPath"]))
+        self.imgAndCommandList = imgAndCommandList
+        self.i = 0
+        self.window = Tk()
+        self.window.title(str(self.imgAndCommandList[self.i]["imgPath"]))
+        self.player = self.Player(self)
 
         # self.inputbox.state("zoomed")
 
-        imgFrame = Image.open(this.Img)
-        imgFrameResized = imgFrame.resize((600, 600), Image.ANTIALIAS)
-        itkFrame = ImageTk.PhotoImage(imgFrameResized)
-        lFrame = Label(window, image=itkFrame)
-        lFrame.grid(row=0, columnspan=3)
+        imgFrame = Image.open(self.imgAndCommandList[self.i]["imgPath"])
+        # imgFrame = imgFrame.resize((600, 600), Image.ANTIALIAS)
+        itkFrame = ImageTk.PhotoImage(imgFrame)
+        self.lImgFrame = Label(self.window, image=itkFrame)
+        self.lImgFrame.grid(row=0, columnspan=3)
 
-        lVelX = Label(window, text="Vel. X")
-        self.eVelX = Entry(window)
-        self.eVelX.insert(END, velX)
-        lVelX.grid(row=2, sticky="e")
-        self.eVelX.grid(row=2, column=1, columnspan=2)
+        lVelX = Label(self.window, text="Vel. X")
+        self.eVelX = Entry(self.window)
+        self.eVelX.insert(END, self.imgAndCommandList[self.i]["velX"])
+        lVelX.grid(row=1, sticky="e")
+        self.eVelX.grid(row=1, column=1, columnspan=2)
         self.eVelX.focus_set()
 
-        lVelYaw = Label(window, text="Vel. Yaw")
-        self.eVelYaw = Entry(window)
-        self.eVelYaw.insert(END, velYaw)
-        lVelYaw.grid(row=3, sticky="e")
-        self.eVelYaw.grid(row=3, column=1, columnspan=2)
+        lVelYaw = Label(self.window, text="Vel. Yaw")
+        self.eVelYaw = Entry(self.window)
+        self.eVelYaw.insert(END, self.imgAndCommandList[self.i]["velYaw"])
+        lVelYaw.grid(row=2, sticky="e")
+        self.eVelYaw.grid(row=2, column=1, columnspan=2)
 
-        bForward = Button(window, text="Forward",
-                          width=10, command=self._bForwardClicked)
-        bForward.grid(row=6, column=1)
-
-        bBackward = Button(window, text="Backward",
+        bBackward = Button(self.window, text="Backward",
                            width=10, command=self._bBackwardClicked)
-        bBackward.grid(row=6, column=2)
+        bBackward.grid(row=3, column=0)
 
-        window.mainloop()
+        bForward = Button(self.window, text="Forward",
+                          width=10, command=self._bForwardClicked)
+        bForward.grid(row=3, column=2)
 
-    def _buttonClicked(self, status):
-        self.labelId = self.svLabelId.get()
-        self.bin = self.eBin.get()
-        self.cin = self.eCin.get()
-        self.ean = self.eEan.get()
-        self.quantity = self.eQuantity.get()
-        self.status = status
+        self.bPlayStop = Button(self.window, text="Play",
+                          width=10, command=self._bPlayPausedClicked)
+        self.bPlayStop.grid(row=3, column=1)
+
+        self.window.mainloop()
+
+    def updateView(self):
+        self.window.title(str(self.imgAndCommandList[self.i]["imgPath"]))
+
+        imgFrame = Image.open(self.imgAndCommandList[self.i]["imgPath"])
+        # imgFrame = imgFrame.resize((600, 600), Image.ANTIALIAS)
+        itkFrame = ImageTk.PhotoImage(imgFrame)
+        self.lImgFrame.configure(image=itkFrame)
+        self.lImgFrame.image = itkFrame
+
+        self.eVelX.delete(0, END)
+        self.eVelX.insert(END, self.imgAndCommandList[self.i]["velX"])
+        self.eVelYaw.delete(0, END)
+        self.eVelYaw.insert(END, self.imgAndCommandList[self.i]["velYaw"])
+
+    def forward(self):
+        if self.i < len(self.imgAndCommandList)-1:
+            self.i += 1
+        else:
+            self.i = 0
+        self.updateView()
+
+    def backward(self):
+        if self.i > 0:
+            self.i -= 1
+        else:
+            self.i = len(self.imgAndCommandList)-1
+        self.updateView()
 
     def _bForwardClicked(self):
-        self._buttonClicked("forward")
+        self.forward()
 
     def _bBackwardClicked(self):
-        self._buttonClicked("backward")
+        self.backward()
+
+    def _bPlayPausedClicked(self):
+        if self.player.isPlaying():
+            self.player.stop()
+            self.bPlayStop.config(text="Play")
+        else:
+            self.player.play()
+            self.bPlayStop.config(text="Stop")
+
+    class Player(Thread):
+        def __init__(self, imgAndCmdWindow):
+            self.imgAndCmdWindow = imgAndCmdWindow
+            self.playing = False
+
+        def play(self):
+            self.playing = True
+            self.run()
+
+        def stop(self):
+            self.playing = False
+
+        def isPlaying(self):
+            return self.playing
+
+        def run(self):
+            if self.playing:
+                self.imgAndCmdWindow.forward()
+                time.sleep(0.5)
+                #self.run()
+
 
 
 '''

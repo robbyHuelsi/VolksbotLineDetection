23495a91911f84c3ac43300701621b174c1449f5
Florian Schwab <florian.c.schwab@googlemail.com>
2018-07-04 00:19:29 +0200

    Added AddLayer besides MultLayer

--- train/analysisFunctions.py
+++ train/analysisFunctions.py
@@ -161,6 +161,20 @@ class MultLayer(Layer):
         return input_shape
 
 
+class AddLayer(Layer):
+    def __init__(self, **kwargs):
+        super(AddLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        super(AddLayer, self).build(input_shape)
+
+    def call(self, x, **kwargs):
+        return x + kwargs["y"]
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
 def build_salient_model(our_model):
     activation_layers = get_layers(our_model, "relu")
 
@@ -174,7 +188,7 @@ def build_salient_model(our_model):
         if current.get_shape()[1] != previous.get_shape()[1]:
             previous = UpsamplingLayer()(previous)
 
-        activation_layers[i]["mean_actv"] = MultLayer()(previous, y=current)
+        activation_layers[i]["mean_actv"] = AddLayer()(previous, y=current)
 
     last = UpsamplingLayer()(activation_layers[0]["mean_actv"])
     model = Model(inputs=our_model.layers[0].output, outputs=last)
@@ -192,8 +206,6 @@ if __name__ == '__main__':
     analysis_parser.add_argument("--data_dir", action="store", type=str, default=None)
     analysis_parser.add_argument("--img_file", action="store", type=str, default=None)
     args = analysis_parser.parse_args()
-
-    assert args.data_dir
 
     ref_model = None
     our_model = None
@@ -260,6 +272,7 @@ if __name__ == '__main__':
             plt.imshow(pred[0, :, :, 0], cmap="plasma")
             plt.show()
         else:
+            assert args.data_dir
             ibg = ImageBatchGenerator(args.data_dir, preprocess_input=preprocess_input, labeled=False, shuffle=False,
                                       crop=False, batch_size=1)
             preds = model.predict_generator(ibg, verbose=1)
